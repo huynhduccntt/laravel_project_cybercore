07f21cdf3bdc81bf64308caeb61dde9944a4213d
HuynhNgocDuc <huynhduc.cntt@gmail.com>
2019-04-06 12:24:29 +0700

    Update2019

--- resources/views/frontEnd/includes/project_home.blade.php
+++ resources/views/frontEnd/includes/project_home.blade.php
@@ -14,31 +14,22 @@
             </div>
             <div class="col-md-3 col-lg-3 mb-4 mb-lg-3">
                 <div class="h-entry">
-                    <img src="images/hero_1.jpg" alt="Image" class="img-fluid rounded">
-                    <h2 class="font-size-regular"><a href="#" class="text-black">Many People Selling Online</a></h2>
-                    <div class="meta mb-3">by Mark Spiker<span class="mx-1">•</span> Jan 18, 2019 <span class="mx-1">•</span> <a href="#">News</a></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+                    <img src="https://cybercore.vn/wp-content/uploads/2018/08/IMG_2341-1024x683.jpg" alt="Image" class="img-fluid rounded">
                 </div>
             </div>
             <div class="col-md-3 col-lg-3 mb-4 mb-lg-3">
                 <div class="h-entry">
-                    <img src="images/hero_1.jpg" alt="Image" class="img-fluid rounded">
-                    <h2 class="font-size-regular"><a href="#" class="text-black">Many People Selling Online</a></h2>
-                    <div class="meta mb-3">by Mark Spiker<span class="mx-1">•</span> Jan 18, 2019 <span class="mx-1">•</span> <a href="#">News</a></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+                    <img src="https://cybercore.vn/wp-content/uploads/2018/08/IMG_2341-1024x683.jpg" alt="Image" class="img-fluid rounded">
                 </div>
             </div>
             <div class="col-md-3 col-lg-3 mb-4 mb-lg-3">
                 <div class="h-entry">
-                    <img src="images/hero_1.jpg" alt="Image" class="img-fluid rounded">
-                    <h2 class="font-size-regular"><a href="#" class="text-black">Many People Selling Online</a></h2>
-                    <div class="meta mb-3">by Mark Spiker<span class="mx-1">•</span> Jan 18, 2019 <span class="mx-1">•</span> <a href="#">News</a></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+                    <img src="https://cybercore.vn/wp-content/uploads/2018/08/IMG_2341-1024x683.jpg" alt="Image" class="img-fluid rounded">
                 </div>
             </div>
 
             <div class="col-12 text-center mt-4">
-                <a href="#" class="btn btn-primary rounded py-2 px-4 text-white">View All Posts</a>
+                <a href="#" class="btn btn-primary rounded py-2 px-4 text-white">Xem tất cả</a>
             </div>
         </div>
     </div>
